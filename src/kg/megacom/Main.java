53cff1bfa5bb0bd4b62fe6fe0d39cfa71e1143da
Nursultan <nursultankudajkulov11@gmail.com>
2021-05-14 22:04:12 +0600

    initial commit

---
+++ src/kg/megacom/Main.java
@@ -0,0 +1,31 @@
+package kg.megacom;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student harryPotter = new Student("Harry Potter", "+996555555555", true);
+        Student hermione = new Student("Hermione", "+9965559844566", false);
+        Student ronWisley = new Student("Ron Wisley", "+996555753495", false);
+        Teacher SeverusSnape = new Teacher("Severus Snape", 120000);
+
+        Course course = new Course();
+        course.name = "Poisons";
+
+        course.studentsCount(15);
+        course.addStudent(harryPotter);
+        course.addStudent(hermione);
+        course.addStudent(ronWisley);
+
+        course.addTeacher(SeverusSnape);
+
+        course.removeStudent("+996555753495", ronWisley);
+
+        course.print(course);
+
+        System.out.println("Students: " + Arrays.toString(course.students));
+        System.out.println("Teacher: " + Arrays.toString(new Teacher[]{course.teacher}));
+    }
+}
---
+++ src/kg/megacom/Student.java
@@ -0,0 +1,17 @@
+package kg.megacom;
+
+public class Student {
+    String fio;
+    String phoneNumber;
+    boolean isPaid;
+
+    public Student(String fio, String phoneNumber, boolean isPaid) {
+        this.fio = fio;
+        this.phoneNumber = phoneNumber;
+        this.isPaid = isPaid;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+}
